9e203cdd922623c898c84d6a950f5bf9693c6d62
Gustavo Lozano <gal@Gustavo-Lozano.local>
2016-12-13 10:58:05 -0500

    initial commit

---
+++ grails-app/domain/kahuu/anuncios/Anuncio.groovy
@@ -0,0 +1,46 @@
+package kahuu.anuncios
+
+/**
+ * Representa un anuncio o propaganda
+ * @author gustavolozano
+ */
+class Anuncio implements Comparable
+{
+	String titulo;
+	
+	String urlWebsite;
+	
+	String descripcion;
+	
+	double costoPClick;
+	
+	double presupuesto;
+	
+	int numeroClicks;
+	
+	int numeroVistas;
+	
+	boolean estado;
+	
+	byte[] image;
+	
+	Date fechaCreado;
+	
+	static hasMany = [vistas:Vista];	
+		
+    static constraints = 
+	{
+		descripcion size: 2..150, blank: false;
+		titulo size: 2..45, blank: false;
+		urlWebsite blank: false, url:true;
+	}
+
+	@Override
+	public int compareTo(Object o) 
+	{
+		double primero = ((numeroClicks/numeroVistas) * 100) * costoPClick;
+		double segundo = ((o.numeroClicks/o.numeroVistas) * 100) * o.costoPClick
+		
+		return segundo.compareTo(primero);
+	}
+}
